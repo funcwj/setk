04b819a1785aa8022259cba3af676e058db2b0d8
funcwj <jianwu.wujian@alibaba-inc.com>
2018-03-30 15:46:40 +0800

    restore previous mask computation logic

--- src/compute-masks.cc
+++ src/compute-masks.cc
@@ -11,18 +11,17 @@
 using namespace kaldi;
 
 void ComputeMasks(ShortTimeFTComputer &stft_computer, 
-                      const MatrixBase<BaseFloat> &noisy_data,
+                      const MatrixBase<BaseFloat> &noise_data,
                       const MatrixBase<BaseFloat> &clean_data,
                       std::string &type,
                       Matrix<BaseFloat> *mask) {
 
     Matrix<BaseFloat> numerator, denumerator;
     stft_computer.Compute(clean_data, NULL, &numerator, NULL);
-    stft_computer.Compute(noisy_data, NULL, &denumerator, NULL);
+    stft_computer.Compute(noise_data, NULL, &denumerator, NULL);
 
     if (type == "irm" || type == "wiener") {
-        // denumerator is noisy, so do not need to add them
-        // denumerator.AddMat(1, numerator);
+        denumerator.AddMat(1, numerator);
         numerator.DivElements(denumerator);
     }
 
@@ -40,18 +39,14 @@ int main(int argc, char *argv[]) {
             "Compute T-F mask(using for speech enhancement)\n"
             "\n"
             "For ratio | wiener mask\n"
-            "Usage:  compute-masks [options...] <noisy-rspecifier> <clean-rspecifier> <mask-wspecifier>\n"
-            "   or:  compute-masks [options...] <noisy-rxfilename> <clean-rxfilename> <mask-wxfilename>\n"
-            "By default, this command compute clean masks, to compute noise part, using <noise-rspecifier> instead\n"
-            "\n"
-            "For binary mask\n"
             "Usage:  compute-masks [options...] <noise-rspecifier> <clean-rspecifier> <mask-wspecifier>\n"
-            "   or:  compute-masks [options...] <noise-rxfilename> <clean-rxfilename> <mask-wxfilename>\n";
+            "   or:  compute-masks [options...] <noise-rxfilename> <clean-rxfilename> <mask-wxfilename>\n"
+            "By default, this command compute clean masks, to compute noise part, using <noise-rspecifier> instead\n";
 
         ParseOptions po(usage);
         ShortTimeFTOptions stft_options;
 
-        bool wx_binary = false, enable_scale = false;
+        bool wx_binary = false;
         std::string mask_type = "irm", window = "hamming";
         BaseFloat frame_shift = 256, frame_length = 1024;
 
@@ -60,7 +55,6 @@ int main(int argc, char *argv[]) {
         po.Register("frame-shift", &frame_shift, "Frame shift in number of samples");
         po.Register("frame-length", &frame_length, "Frame length in number of samples");
         po.Register("window", &window, "Type of window(\"hamming\"|\"hanning\"|\"blackman\"|\"rectangular\")");
-        // po.Register("enable-scale", &enable_scale, "Let inf-norm of sample vector be one");
 
         po.Read(argc, argv);
 
@@ -72,17 +66,17 @@ int main(int argc, char *argv[]) {
         if (mask_type != "irm" && mask_type != "ibm" && mask_type != "wiener")
             KALDI_ERR << "Unknown arguments for --mask: " << mask_type;
 
-        std::string noisy_in = po.GetArg(1), clean_in = po.GetArg(2), mask_out = po.GetArg(3);
+        std::string noise_in = po.GetArg(1), clean_in = po.GetArg(2), mask_out = po.GetArg(3);
         
-        bool noisy_is_rspecifier = (ClassifyRspecifier(noisy_in, NULL, NULL) != kNoRspecifier),
+        bool noise_is_rspecifier = (ClassifyRspecifier(noise_in, NULL, NULL) != kNoRspecifier),
              clean_is_rspecifier = (ClassifyRspecifier(clean_in, NULL, NULL) != kNoRspecifier),
              mask_is_wspecifier = (ClassifyWspecifier(mask_out, NULL, NULL, NULL) != kNoWspecifier);
 
-        if (noisy_is_rspecifier != mask_is_wspecifier)
+        if (noise_is_rspecifier != mask_is_wspecifier)
             KALDI_ERR << "Cannot mix archives with regular files";
 
-        if (noisy_is_rspecifier != clean_is_rspecifier)
-            KALDI_ERR << "Configure with noisy/clean must keep same";
+        if (noise_is_rspecifier != clean_is_rspecifier)
+            KALDI_ERR << "Configure with noise/clean must keep same";
 
         // config power
         if (mask_type == "wiener")
@@ -95,8 +89,8 @@ int main(int argc, char *argv[]) {
 
         ShortTimeFTComputer stft_computer(stft_options);
 
-        if (noisy_is_rspecifier) {
-            SequentialTableReader<WaveHolder> noisy_reader(noisy_in);
+        if (noise_is_rspecifier) {
+            SequentialTableReader<WaveHolder> noise_reader(noise_in);
             RandomAccessTableReader<WaveHolder> clean_reader(clean_in);
 
             BaseFloatMatrixWriter kaldi_writer;
@@ -104,8 +98,8 @@ int main(int argc, char *argv[]) {
                 KALDI_ERR << "Could not initialize output with wspecifier " << mask_out;
             
             int num_utts = 0, num_no_tgt_utts = 0, num_done = 0;
-            for (; !noisy_reader.Done(); noisy_reader.Next()) {
-                std::string utt_key = noisy_reader.Key();
+            for (; !noise_reader.Done(); noise_reader.Next()) {
+                std::string utt_key = noise_reader.Key();
                 num_utts += 1;
 
                 if (!clean_reader.HasKey(utt_key)) {
@@ -114,11 +108,11 @@ int main(int argc, char *argv[]) {
                     continue;
                 }
 
-                const WaveData &noisy_data = noisy_reader.Value(), 
+                const WaveData &noise_data = noise_reader.Value(), 
                       &clean_data = clean_reader.Value(utt_key);
                     
                 Matrix<BaseFloat> mask;
-                ComputeMasks(stft_computer, noisy_data.Data(), clean_data.Data(), mask_type, &mask);
+                ComputeMasks(stft_computer, noise_data.Data(), clean_data.Data(), mask_type, &mask);
 
                 kaldi_writer.Write(utt_key, mask);
                 num_done++;
@@ -132,17 +126,17 @@ int main(int argc, char *argv[]) {
             return num_done == 0 ? 1: 0;
         } else {
             bool binary;
-            Input kn(noisy_in, &binary);
+            Input kn(noise_in, &binary);
             Input kt(clean_in, &binary);
 
-            WaveData noisy_data, clean_data;
-            noisy_data.Read(kn.Stream());
+            WaveData noise_data, clean_data;
+            noise_data.Read(kn.Stream());
             clean_data.Read(kt.Stream());
 
             Matrix<BaseFloat> mask;
-            ComputeMasks(stft_computer, noisy_data.Data(), clean_data.Data(), mask_type, &mask);
+            ComputeMasks(stft_computer, noise_data.Data(), clean_data.Data(), mask_type, &mask);
             WriteKaldiObject(mask, mask_out, wx_binary);
-            KALDI_LOG << "Done processed " << noisy_in;
+            KALDI_LOG << "Done processed " << noise_in;
         }
 
     } catch(const std::exception& e) {
