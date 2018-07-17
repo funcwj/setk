edcc31ba98881703cff090f81bb841f0c142d207
funcwj <funcwj@foxmail.com>
2018-07-17 18:18:33 +0800

    fix local/prepare_mask.sh

--- local/prepare_mask.sh
+++ local/prepare_mask.sh
@@ -20,9 +20,15 @@ for f in $clean_dir $noisy_dir $noise_dir; do [ ! -d $f ] && echo "**error**: $f
 
 mask_dir=$2 && mkdir -p $mask_dir && mask_dir=$(cd $mask_dir; pwd)
 
-find `cd $noisy_dir; pwd` -name "*.wav" | awk -F '/' '{split($NF, a, "."); printf "%s%s\t%s\n", a[1], a[2], $0}' | sort -k1 > $mask_dir/wav.scp
-find `cd $clean_dir; pwd` -name "*.wav" | awk -F '/' '{split($NF, a, "."); printf "%s%s\t%s\n", a[1], a[2], $0}' | sort -k1 > $mask_dir/clean.scp
-find `cd $noise_dir; pwd` -name "*.wav" | awk -F '/' '{split($NF, a, "."); printf "%s%s\t%s\n", a[1], a[2], $0}' | sort -k1 > $mask_dir/noise.scp
+find `cd $noisy_dir; pwd` -name "*.wav" | \
+    awk -F '/' '{split($NF, a, "."); printf "%s%s\t%s\n", a[1], a[2], $0}' | \
+    sort -k1 > $mask_dir/wav.scp
+find `cd $clean_dir; pwd` -name "*.wav" | \
+    awk -F '/' '{split($NF, a, "."); printf "%s%s\t%s\n", a[1], a[2], $0}' | \
+    sort -k1 > $mask_dir/clean.scp
+find `cd $noise_dir; pwd` -name "*.wav" | \
+    awk -F '/' '{split($NF, a, "."); printf "%s%s\t%s\n", a[1], a[2], $0}' | \
+    sort -k1 > $mask_dir/noise.scp
 
 # utt-level cmvn
 awk '{print $1"\t"$1}' $mask_dir/wav.scp > $mask_dir/utt2spk
