e7d9d31ef8d9ac5a14b524b98c459c23a71428c6
funcwj <funcwj@foxmail.com>
2019-05-16 21:14:24 +0800

    add rir_generate_2d.py

--- scripts/sptk/wav_separate.py
+++ scripts/sptk/wav_separate.py
@@ -37,8 +37,11 @@ def run(args):
     mask_reader = MaskReader[args.fmt](args.mask_scp)
 
     num_done = 0
-    with WaveWriter(args.dst_dir, fs=args.samp_freq) as writer:
+    with WaveWriter(args.dst_dir, fs=args.sf) as writer:
         for key, specs in spectrogram_reader:
+            # if multi-channel, choose ch0
+            if specs.ndim == 3:
+                specs = specs[0]
             # specs: T x F
             if key in mask_reader:
                 num_done += 1
@@ -55,19 +58,21 @@ def run(args):
                         .format(mask.shape, specs.shape))
                 nsamps = spectrogram_reader.nsamps(
                     key) if args.keep_length else None
-                norm = spectrogram_reader.samp_norm(key)
+                norm = spectrogram_reader.samp_norm(
+                    key) if args.mixed_norm else None
                 # use phase from ref
                 if phase_reader is not None:
                     angle = np.angle(phase_reader[key])
                     phase = np.exp(angle * 1j)
-                    samps = istft(
-                        np.abs(specs) * mask * phase,
-                        **stft_kwargs,
-                        norm=norm,
-                        nsamps=nsamps)
+                    samps = istft(np.abs(specs) * mask * phase,
+                                  **stft_kwargs,
+                                  norm=norm,
+                                  nsamps=nsamps)
                 else:
-                    samps = istft(
-                        specs * mask, **stft_kwargs, norm=norm, nsamps=nsamps)
+                    samps = istft(specs * mask,
+                                  **stft_kwargs,
+                                  norm=norm,
+                                  nsamps=nsamps)
                 writer.write(key, samps)
     logger.info("Processed {:d} utterances over {:d}".format(
         num_done, len(spectrogram_reader)))
@@ -79,34 +84,39 @@ if __name__ == "__main__":
         "Command to separate target component from mixtures given Tf-masks",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         parents=[StftParser.parser])
-    parser.add_argument(
-        "wav_scp", type=str, help="Mixture wave scripts in kaldi format")
-    parser.add_argument(
-        "mask_scp",
-        type=str,
-        help="Scripts of masks in kaldi's archive or numpy's ndarray")
-    parser.add_argument(
-        "dst_dir", type=str, help="Location to dump separated wave files")
-    parser.add_argument(
-        "--sample-frequency",
-        type=int,
-        default=16000,
-        dest="samp_freq",
-        help="Waveform data sample frequency")
-    parser.add_argument(
-        "--phase-ref",
-        type=str,
-        default="",
-        help="If assigned, use phase of it instead of mixture")
-    parser.add_argument(
-        "--mask-format",
-        dest="fmt",
-        choices=["kaldi", "numpy"],
-        default="kaldi",
-        help="Define format of masks, kaldi's archives or numpy's ndarray")
-    parser.add_argument(
-        "--keep-length",
-        action="store_true",
-        help="If ture, keep result the same length as orginal")
+    parser.add_argument("wav_scp",
+                        type=str,
+                        help="Mixture wave scripts in kaldi format")
+    parser.add_argument("mask_scp",
+                        type=str,
+                        help="Scripts of masks in kaldi's "
+                        "archive or numpy's ndarray")
+    parser.add_argument("dst_dir",
+                        type=str,
+                        help="Location to dump separated wave files")
+    parser.add_argument("--sample-frequency",
+                        type=int,
+                        default=16000,
+                        dest="sf",
+                        help="Waveform data sample frequency")
+    parser.add_argument("--phase-ref",
+                        type=str,
+                        default="",
+                        help="If assigned, use phase of it "
+                        "instead of mixture")
+    parser.add_argument("--mask-format",
+                        dest="fmt",
+                        choices=["kaldi", "numpy"],
+                        default="kaldi",
+                        help="Define format of masks, kaldi's "
+                        "archives or numpy's ndarray")
+    parser.add_argument("--keep-length",
+                        action="store_true",
+                        help="If ture, keep result the same length as orginal")
+    parser.add_argument("--use-mixed-norm",
+                        action="store_true",
+                        dest="mixed_norm",
+                        help="If true, keep norm of separated "
+                        "same as mixed one")
     args = parser.parse_args()
     run(args)
